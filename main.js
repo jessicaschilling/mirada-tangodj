c98b1b319107952bb948076b7dd8330e8de85cf9
Jessica Schilling <jessica.schilling@libertymutual.com>
2017-12-28 17:23:10 -0500

    End-o-day commit
    Got the menu set up, but need to figure out how to make the js work.
    
    ref #7

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
+const Menu = electron.Menu;
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -58,3 +60,30 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+app.on('ready', function () {
+
+  const menuTemplate = [
+        {
+            label: 'Electron',
+            submenu: [
+                {
+                    label: 'Christmas Style',
+                    click: () => {
+                        console.log('About Clicked');
+                        ['background', 'nowPlaying', 'nextTanda', 'ribbon'].forEach(function( id ) {document.getElementById( id ).className = "themeXmas";});
+                    }
+                }, {
+                    type: 'separator'
+                }, {
+                    label: 'Quit',
+                    click: () => {
+                        app.quit();
+                    }
+                }
+            ]
+        }
+    ];
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
+});
